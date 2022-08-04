85d8fb12ed01b72807abf4c2df59f85cfaffc795
11t518s <11t518s20161759@gmail.com>
2022-08-05 01:09:38 +0900

    chore: prettier, eslint prettier, eslint 설치 및 설정 #1

--- pages/index.js
+++ pages/index.js
@@ -5,5 +5,6 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   const { example } = useContext(ContextApi);
   console.log(example);
+
   return <div className={styles.container}>hello world</div>;
 }
