1e91c6acec77870cdda9c7ef4f25897516f21571
11t518s <11t518s20161759@gmail.com>
2022-08-12 01:32:15 +0900

    fix: splash 반응형과 줄바꿈 수정

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,32 @@
-import { useContext, useEffect, useState } from "react";
-import { ContextApi } from "../Contexts/contextApi";
+import { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
+import { totalText } from "../constant";
 
 export default function Home() {
-  const { example } = useContext(ContextApi);
-  const [outputText, setOutputText] = useState(''); //출력될 텍스트를 저장할 변수 만들어주기
+  const [outputText, setOutputText] = useState(""); //출력될 텍스트를 저장할 변수 만들어주기
   const [count, setCount] = useState(0); // 글자 수 카운터 변수
-  const totalText = '악동 우주먼지들이 등장했어요! 이 작고 귀여운 친구들은 보기와 다르게 아주 괴팍한 취미를 가지고 있어요\n 바로 축제만 열렸다 하면 나타나서 축제를 아주 쑥대밭으로 만든다는 것이죠! 더 개판이 되기 전에 이 우주먼지들을 잡아서 대동제를 지켜주세요! 최강우주전사 아카데미 숭실대학교 친구들이라면 이 악동들을 손쉽게 잡을 수 있을거에요 사례는 섭섭치 않게 할게요! 그럼 모두 행운을 빌어요';
+  const scrollRef = useRef();
 
-  useEffect(()=> {
-    const textInterval = setInterval(()=> {
+  useEffect(() => {
+    const textInterval = setInterval(() => {
+      if (count < totalText.length) {
+        scrollRef.current.scrollIntoView({
+          behavior: "smooth",
+          block: "end",
+          inline: "nearest",
+        });
+      }
       setOutputText((prev) => {
-        let res = prev ? prev + totalText[count] : totalText[0]; //이전 값과 현재의 count가 위치한 인덱스의 글자를 더해 리턴하는 목적
-      
-        setCount((count)+1);
+        const res = prev
+          ? prev + totalText.slice(count - 1, count + 1)
+          : totalText[0]; //이전 값과 현재의 count가 위치한 인덱스의 글자를 더해 리턴하는 목적
 
-        if(count >= totalText.length) {
-          setCount(0);
-          setOutputText('');
+        if (count >= totalText.length) {
+          return totalText;
+        } else {
+          setCount(count + 2);
+          return res;
         }
-      
-        return res;
       });
     }, 200);
     return () => {
@@ -28,13 +34,12 @@ export default function Home() {
     };
   });
 
-  console.log(example);
-
-  return <div className={styles.background}>
-    <div className={styles.container}>
-      <img className={styles.logo_img}/>
-      <div className={styles.content}>{outputText}</div>
+  return (
+    <div className={styles.background}>
+      <div ref={scrollRef} className={styles.container}>
+        <div className={styles.logo_img} />
+        <div className={styles.content}>{outputText}</div>
+      </div>
     </div>
-  </div>;
-  
+  );
 }
