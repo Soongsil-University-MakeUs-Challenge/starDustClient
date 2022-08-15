c009b75738372979b3f53884320895497740a660
최봉수 <derrick@whoyaho.com>
2022-08-15 12:15:59 +0900

    study: useState 유즈 스테이트 쓰는 방법 여러가지로 배움 #6

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,4 +1,30 @@
-export default function Test() {
+import { useState } from 'react'
 
-  return <div>test</div>;
+const Test = () => {
+  const [sso, setSso] = useState('sssssssssssssssssssssso')
+  const [visible, setVisible] = useState(false)
+
+  const handleSsoNameChange = (text) => {
+    setSso(text.target.value)
+  }
+
+  const handleVisible = () => {
+    // setVisible(true)
+    // setVisible(!visible)
+    setVisible((prev) => {
+      console.log(prev)
+      return !prev
+    })
+  }
+
+  return (
+    <>
+      {sso}
+      <input onChange={handleSsoNameChange} />
+      <button onClick={handleVisible}>보여요 토글 버튼</button>
+      {visible && <div>보여요</div>}
+    </>
+  )
 }
+
+export default Test
--- pages/testa.js
+++ pages/testa.js
@@ -1,5 +1,3 @@
 export default function Twiaksdjflajdsvlkahsjdflaskjesta() {
-
-    return <div>testa</div>;
-  }
-  
+  return <div>testa</div>
+}
