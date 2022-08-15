cda94eafcb209447ca5ac64f45e7036ddb645b81
최봉수 <derrick@whoyaho.com>
2022-08-15 13:02:41 +0900

    study: map, filter 어레이를 다루는 법을 배움 #6

---
+++ constant/array.js
@@ -0,0 +1,5 @@
+export const food = [
+  { id: 0, name: '닭강정' },
+  { id: 1, name: '콜라' },
+  { id: 2, name: '사이다' },
+]
--- pages/test/_components/Yeon.jsx
+++ pages/test/_components/Yeon.jsx
@@ -1,8 +1,17 @@
+import { food } from '../../../constant/array'
+
+const test = food.map((item) => <div key={item.id}>{item.name}</div>)
 const Yeon = ({ banana }) => {
+  const filteredFood = food.filter((item) => item.id > 0)
   return (
     <>
       <div>--------------------</div>
       <div>{banana}</div>
+      {test}
+
+      {filteredFood.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
       <div>--------------------</div>
     </>
   )
