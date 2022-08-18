5251c627ce57abf89e85f529ece343a87199d2d2
hongSso <soyunsarang@naver.com>
2022-08-18 23:42:10 +0900

    wip: 로그인 화면 전환되는 component 작업 #14

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,33 +1,40 @@
+import { func } from "prop-types";
 import { useEffect, useRef, useState } from "react";
 import styles from "../../styles/Login.module.css";
 
 
 export default function Login(){
-
-    const[codeNumber,setCodeNumber]=useState("");
-    const onChange = (event) => {
-        console.log(event.target.value);
+    const [codeNumber, setCodeNumber] = useState("");
+    const onChange = (e) => {
+        const { name, value } = e.target
+        console.log(name, value)
+        setCodeNumber( value )
     }
-return(
-    <div className={styles.background}>
-        <div className={styles.logo_img}/>
-            <div><a className={styles.inputrequest}>Input your code number...</a>
-                <div>
-                    <input
-                        id="id"
-                        type="text"
-                        value={codeNumber}
-                        className={styles.inputBox}
-                        onChange={onChange}
-                        placeholder="code number"/>
-                    <div className={styles.prove}>
-                        <a><span style={{color:"#F9E219"}}>학번</span>으로 대숭실대 우주전사임을 증명해주세요!</a>
+
+    return(
+        <div className={styles.background}>
+            <div className={styles.logo_img}/>
+                <div><a className={styles.inputrequest}>Input your code number...</a>
+                    <div>
+                        <input
+                            name="student-id"
+                            type="text"
+                            value={codeNumber}
+                            className={styles.inputBox}
+                            onChange={onChange}
+                            placeholder="code number"/>
+                            
+                        <div className={styles.prove}>
+                            <a><span style={{color:"#F9E219"}}>학번</span>으로 대숭실대 우주전사임을 증명해주세요!</a>
+                        </div>
                     </div>
+                    
                 </div>
-                
-            </div>
-            <div className={styles.loginBtn}><a href="/login">Login{">"}</a></div>
-            <div className={styles.ssumcBtn}><a>SSUMC</a></div>
-    </div>  
-)
+                <div className={styles.loginBtn}><a href="/login">Login{">"}</a></div>
+                <div className={styles.ssumcLogo}><a>SSUMC</a></div>
+                <div className={styles.startrequest}><a>Do you want to go <br/> hunting stardust?</a></div>
+                <div><button className={styles.goBtn}>Go!</button></div>
+                <div><button className={styles.awayBtn}>Run away?</button></div>
+        </div>  
+    )
 }
