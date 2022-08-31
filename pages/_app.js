8097b641622ce204e7aa8d44ea1eab4726591e67
11t518s <11t518s20161759@gmail.com>
2022-09-01 00:16:42 +0900

    fix: kakao map error api 키를 _documents로 이동, next 버전 변경 #15

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <ContextStore>
-      <Script src="//dapi.kakao.com/v2/maps/sdk.js?appkey=1664a34e52ea5802af4d289cbdeef3ed&libraries=services,clusterer&autoload=false"
+      <Script src=""
               strategy="beforeInteractive"
               />
       <Component {...pageProps} />
