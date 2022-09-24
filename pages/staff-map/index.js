69ad3c25a18a5a41bf18fbe60e6642161dd74b61
minuxx <minux8905@gmail.com>
2022-09-24 22:13:28 +0900

    feat: 기기 현재 좌표 가져오기 기능 추가

--- pages/staff-map/index.js
+++ pages/staff-map/index.js
@@ -2,29 +2,43 @@ import { useRouter } from "next/router";
 import Link from 'next/link'
 import KakaoAPIMap from "./_components/StaffMap";
 import styles from "../../styles/StaffMap.module.css";
+import { useState, useEffect } from "react";
 
 export default function StaffMapPage() {
-    const router = useRouter()
-    const [myLocation, setMyLocation] = useState('');
+    const [myLocation, setMyLocation] = useState({ "latitude": 37.496313, "longitude": 126.957037 });
   
     useEffect(() => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-          setMyLocation({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          });
-        });
-      } else {
-        window.alert('현재 위치를 알 수 없어 기본 위치로 지정합니다.');
-        setMyLocation({ latitude: 37.4862618, longitude: 127.1222903 });
-      }
+      const timerId = setInterval(() => {
+        setCurLocation()
+      }, 1000)
+      return () => clearTimeout(timerId)
     }, []);
-  
-    useEffect(() => {
-        const [latitude, longitude] = myLocation
-        console.log(`위도 ${latitude}, 경도: ${longitude}`);
-    });
+
+    const setCurLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+              setMyLocation({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+              });
+            });
+
+            // console.log(`타이머 => 현재 위도: ${myLocation.latitude}, 현재 위도: ${myLocation.longitude}`)
+        } else {
+          console.log("먼지좌표 업데이트 실패")
+        }
+    }
+
+    const onClickSendLocation = async () => {
+        // console.log(`클릭 => 현재 위도: ${myLocation.latitude}, 현재 위도: ${myLocation.longitude}`)
+        // const response = await sendDustLocation(latitude, longitude)
+
+        // if (response.status == 200) {
+        //     console.log("먼지 좌표 전송 성공")
+        // } else {
+        //     console.log("먼지 좌표 전송 실패")
+        // }
+    }
 
     return (
         <>
@@ -32,7 +46,7 @@ export default function StaffMapPage() {
             <div className={styles.container}>
                 <div className={styles.dust_box}><div className={styles.dust_black}></div></div>
                 <div className={styles.stretch}></div>
-                <div className={styles.send_location}></div>
+                <div className={styles.send_location} onClick={onClickSendLocation}></div>
             </div>
         </>
     )
