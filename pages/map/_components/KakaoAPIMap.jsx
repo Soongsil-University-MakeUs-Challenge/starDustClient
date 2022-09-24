ead63c413bf7df9ab041db6d5b3fd45b1a445385
minuxx <minux8905@gmail.com>
2022-09-24 16:45:12 +0900

    refact: 코드정리

--- pages/map/_components/KakaoAPIMap.jsx
+++ pages/map/_components/KakaoAPIMap.jsx
@@ -45,10 +45,7 @@ const dustMapper = {
 }
 
 export const KakaoAPIMap = () => {
-  const mapRef = useRef(null);
   const [myLocation, setMyLocation] = useState('');
-  const latitude=0, longitude=0;
-  const currentPosition=[];
 
   useEffect(() => {
     // geolocation 이용 현재 위치 확인, 위치 미동의 시 기본 위치로 지정
@@ -57,7 +54,6 @@ export const KakaoAPIMap = () => {
         setMyLocation({
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
-      
         });
       });
     } else {
@@ -67,14 +63,9 @@ export const KakaoAPIMap = () => {
   }, []);
 
   useEffect(() => {
-    if (typeof myLocation !== 'string') {
-      // 현재 위치 추적
-      currentPosition = [myLocation.latitude, myLocation.longitude];
-      console.log(currentPosition[0], currentPosition[1]);
-    }
-
+    const {latitude, longitude} = myLocation;
+    console.log(latitude, longitude);
   });
-
 
   return (
     <Map center={{ lat: 37.496313, lng: 126.957037 }}style={{ width: "100%", height: "100vh" }}>
@@ -84,31 +75,8 @@ export const KakaoAPIMap = () => {
             <div className={dustMapper[dust.dustId]}></div>
           </CustomOverlayMap>
       })}
-
-      {/* <CustomOverlayMap position={{ lat: 37.495626, lng: 126.9548168 }}>
-        <div className={styles.dust_black}></div>
-
-        <Image src="/img-dust-black.svg" width={30} height={30} alt="dust-black-image" />
-      </CustomOverlayMap>
-
-      <CustomOverlayMap position={{ lat: 37.4954321, lng: 126.9596122 }}>
-        <div className={styles.dust_orange}></div>
-      </CustomOverlayMap>
-
-      <CustomOverlayMap position={{ lat: 37.4957653, lng: 126.9561681 }}>
-        <div className={styles.dust_green}></div>
-      </CustomOverlayMap>
-
-      <CustomOverlayMap position={{ lat: 37.4972478, lng: 126.9562425 }}>
-        <div className={styles.dust_purple}></div>
-      </CustomOverlayMap>
-
-      <CustomOverlayMap position={{ lat: 37.4965255, lng: 126.9551486 }}>
-        <div className={styles.dust_white}></div>
-      </CustomOverlayMap> */}
     </Map>
   )
 };
-//!4d
 
 export default KakaoAPIMap;
--- pages/map/index.js
+++ pages/map/index.js
@@ -6,35 +6,27 @@ import {useRouter} from 'next/router'
 
 export default function Map() {
     const router = useRouter();
-
     
     return (
-    <>
-        <KakaoAPIMap/>
-        <div className={styles.stardust_container}>
-            <div className={styles.stardust_black}></div>
-            <div className={styles.stardust_yellow}></div>
-            <div className={styles.stardust_blue}></div>
-            <div className={styles.stardust_purple}></div>
-            <div className={styles.stardust_red}></div>
-        
-        </div>
+        <>
+            <KakaoAPIMap/>
 
-        <div className={styles.map_logo}></div>
-
-        <div className={styles.button_container}>
-            <div className={styles.location_btn}></div>
-            <div className={styles.refresh_btn}></div>
-            <div className={styles.guide_btn}><Link href="/"><a></a></Link></div>
-            <div className={styles.qr_btn} onClick={()=>router.push('/../qrcode')}>
-                
+            <div className={styles.stardust_container}>
+                <div className={styles.stardust_black}></div>
+                <div className={styles.stardust_yellow}></div>
+                <div className={styles.stardust_blue}></div>
+                <div className={styles.stardust_purple}></div>
+                <div className={styles.stardust_red}></div>
             </div>
 
-        </div>
-        
+            <div className={styles.map_logo}></div>
+                <div className={styles.button_container}>
+                    <div className={styles.location_btn}></div>
+                    <div className={styles.refresh_btn}></div>
+                    <div className={styles.guide_btn}><Link href="/"><a></a></Link></div>
+                    <div className={styles.qr_btn} onClick={()=>router.push('/../qrcode')}>
+                </div>
+            </div>
         </>
-        
     );
-
-    
 }
