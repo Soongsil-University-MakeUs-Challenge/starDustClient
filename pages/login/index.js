a4a0c12f36c16ea4824671f1a02537e7e3023298
minuxx <minux8905@gmail.com>
2022-09-24 19:19:56 +0900

    feat: 닉네임 벨리데이션 추가

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,21 +1,24 @@
 import Link from "next/link";
 import { func } from "prop-types";
 import { useEffect, useRef, useState } from "react";
+import { login } from "../../apis";
 import styles from "../../styles/Login.module.css";
 
 export default function Login() {
-  const [codeNumber, setCodeNumber] = useState("");
+  const [nickname, setNickname] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [nicknameError, setNicknameError] = useState("");
+  const [phoneNumberError, setPhoneNumberError] = useState("");
 
   const onNicknameChange = (e) => {
     const { name, value } = e.target;
-    console.log(name, value);
-    setCodeNumber(value);
+    //console.log(name, value);
+    setNickname(value);
   };
 
   const onPhoneChange = (e) => {
     const { name, value } = e.target;
-    console.log(name, value);
+    //console.log(name, value);
     setPhoneNumber(value);
   };
 
@@ -26,6 +29,32 @@ export default function Login() {
   const onLoginChange = () => {
     setIsLoginSuccess((prev) => !prev);
   };
+
+  const onClickLogin = async (isSuccess) => {
+    console.log(nickname + phoneNumber)
+    const regexNickname = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|]+$/
+
+    // 벨리데이션
+    if (!(nickname.length >= 1 && nickname.length <= 8 && regexNickname.test(nickname))) {
+      setNicknameError("숫자, 특수문자를 제외한 1~8글자를 입력해주세요")
+    } else {
+      setNicknameError("")
+    }
+    
+    if( phoneNumber.length == 0) {
+      setPhoneNumberError("휴대폰번호가 올바르지 않아요")
+    } else {
+      setPhoneNumberError("")
+    }
+    
+    //const response = await login(codeNumber, phoneNumber)
+      
+    if (isSuccess) {
+        setIsLoginSuccess((prev) => !prev);
+    } else {
+    
+    }
+  }
 
   const onByeChange = () => {
     setIsBye((prev) => !prev);
@@ -51,14 +80,14 @@ export default function Login() {
                 <input
                   name="student-nickname"
                   type="text"
-                  value={codeNumber}
+                  value={nickname}
                   className={styles.inputBox}
                   onChange={onNicknameChange}
                   placeholder="nickname"
                 />
-                {isError && (
+                {nicknameError && (
                   <div className={styles.errormessage}>
-                    숫자, 특수문자를 제외한 1~8글자를 입력해주세요
+                    {nicknameError}
                   </div>
                 )}
                 <div className={styles.inputrequest}>
@@ -74,13 +103,13 @@ export default function Login() {
                     onChange={onPhoneChange}
                     placeholder="phone number"
                   />
-                  {isError && (
+                  {phoneNumberError && (
                     <div className={styles.errormessage}>
-                      휴대폰번호가 올바르지 않아요
+                      {phoneNumberError}
                     </div>
                   )}
                 </div>
-                <div className={styles.loginBtn} onClick={onLoginChange}>
+                <div className={styles.loginBtn} onClick={() => onClickLogin(false)}>
                   Login{">"}
                 </div>
               </div>
