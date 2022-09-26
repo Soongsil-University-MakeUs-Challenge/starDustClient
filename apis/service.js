a104bea1da4c7676da34b32e4f5541af67bb1136
jimin <moana16@naver.com>
2022-09-27 00:54:29 +0900

    feature/map-logic 먼지잡기API 추가 #15

--- apis/service.js
+++ apis/service.js
@@ -8,7 +8,7 @@ export const statDustAPI = axios.create({
       accept: "application/json",
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
-    //   "ACCESS-TOKEN": getToken() ?? ''
+      "X-ACCESS-TOKEN": "eyJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VySWQiOjE4LCJpYXQiOjE2NjQyMDMwNDUsImV4cCI6MTY2NDgwNzg0NX0.ydZ7EOtz1_MdMpmYLTJNVuLmGyQ3VXkoOaaobtrEXzU"
     },
 });
 
@@ -20,6 +20,6 @@ export async function sendDustLocation(latitude, longitude) {
   return await statDustAPI.post('/dust/location', { latitude, longitude })
 }
 
-export async function postcatchedDust() {
+export async function postcatchedDust(dustNum) {
   return await statDustAPI.post(`/catch/dust`,{dustNum})
 }
--- pages/qrcode/index.js
+++ pages/qrcode/index.js
@@ -1,43 +1,40 @@
 import React, {useState, useEffect} from "react";
 import { QrReader } from "react-qr-reader";
+import { postcatchedDust } from "../../apis/service";
+import { useRouter } from "next/router";
+
 
 export default function QrcodeScanner () {
+    const router = useRouter();
     const [data, setData] = useState('No result');
     const [hasRendered, setHasRendered] = useState(false);
-    
-
     useEffect(() => {
         setHasRendered(true)
       }, [])
 
-      const postDustNum = async(dustNum) => {
+       const postDustNum = async(dustNum) => {
         const response = await postcatchedDust(dustNum)
-
+        
         if(response.status==200) {
-            if(response.body.code == 200) {
-                //router.back() //map화면으로 다시 돌아가게 해줘야함 근데이게맞아?ㅋ
-                //
-
+            if(response.data.code == 200) {
+                router.replace("/../map")
             }
         }
-      }
+    }
 
-      const dustMapper = {
-        1: styles.dust_black,
-        2: styles.dust_white,
-        3: styles.dust_green,
-        4: styles.dust_orange,
-        5: styles.dust_purple,
-      };
 
     return (
         <>
             {hasRendered && (
                 <QrReader
+                    constraints={{
+                        facingMode: 'environment'
+                    }}
                     onResult={(result, error) => {
                     if (!!result) {
                         setData(result?.text);
-                        postDustNum(Number(data));
+                        postDustNum(Number(result.text));
+                        
 
                     }
 
