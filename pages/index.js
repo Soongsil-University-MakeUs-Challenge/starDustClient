3f0355bdc9cb6c645e24582f2e17d802f5549b07
11t518s <11t518s20161759@gmail.com>
2022-09-27 17:42:29 +0900

    fix deploy

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
 import { totalText } from "../constant";
+import Link from "next/link";
 
 export default function Home() {
   const [outputText, setOutputText] = useState(""); //출력될 텍스트를 저장할 변수 만들어주기
@@ -42,7 +43,7 @@ export default function Home() {
         <div className={styles.logo_img} />
         <div className={styles.content}>{outputText}</div>
       </div>
-      <div className={styles.skipBtn}><a href="/">SKIP{">"}</a></div>
+      <div className={styles.skipBtn}><Link href="/">SKIP{">"}</Link></div>
     </div>
   );
 }
